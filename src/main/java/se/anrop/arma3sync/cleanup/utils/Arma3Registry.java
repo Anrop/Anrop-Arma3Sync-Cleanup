1247cdcddce6d98c8263a77e680f41cf5940ef0a
Björn Dahlgren <bjorn@dahlgren.at>
2017-02-05 16:13:49 +0100

    Get Arma 3 folder path from registry

---
+++ src/main/java/se/anrop/arma3sync/cleanup/utils/Arma3Registry.java
@@ -0,0 +1,36 @@
+package se.anrop.arma3sync.cleanup.utils;
+
+import com.github.sarxos.winreg.HKey;
+import com.github.sarxos.winreg.RegistryException;
+import com.github.sarxos.winreg.WindowsRegistry;
+
+/**
+ * Created by bjorn on 2017-02-05.
+ */
+public class Arma3Registry {
+    private static String[] REGISTRY_PATHS = new String[] {
+            "SOFTWARE\\Wow6432Node\\Bohemia Interactive Studio\\ArmA 3",
+            "SOFTWARE\\Bohemia Interactive Studio\\ArmA 3",
+            "SOFTWARE\\Wow6432Node\\Bohemia Interactive\\ArmA 3",
+            "SOFTWARE\\Bohemia Interactive\\ArmA 3",
+    };
+    private static String REGISTY_VALUE_NAME = "MAIN";
+
+    public static String getPath() {
+        WindowsRegistry reg = WindowsRegistry.getInstance();
+        for (String registryPath : REGISTRY_PATHS) {
+            try {
+                String value = reg.readString(HKey.HKLM, registryPath, REGISTY_VALUE_NAME);
+
+                if (value != null) {
+                    return value;
+                }
+            } catch (RegistryException e) {
+                //e.printStackTrace();
+            }
+        }
+
+        return null;
+    }
+
+}
